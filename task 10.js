babad6031ec777ce7417c5be5ae2f9d966ba5ee1
Kavanatsu <20062000@inbox.ru>
2024-01-16 16:18:39 +0700

    task 10 done

--- task 10.js
+++ task 10.js
@@ -1,6 +1,6 @@
 const twoDimensionalArray = [[1, 4, 5], [1, 3, 4], [2, 6]];
 
-let oneDimensionalArray = [1,4,5,1,3,4,2,6];
+let oneDimensionalArray = [];
 for (let i = 0; i < twoDimensionalArray.length; i++) {
   for (let j = 0; j < twoDimensionalArray[i].length; j++) {
     oneDimensionalArray.push(twoDimensionalArray[i][j]);
